addc7e79920afbef5a936b0df536bb8d5f71af99
Victor Vasiliev <vasilvv@mit.edu>
2013-08-21 18:12:25 -0400

    Add a setup script.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='discuss',
+      version='1.0',
+      description='Python client for Project Athena forum system',
+      author='Victor Vasiliev',
+      packages=['discuss']
+    )
