116a9574b779825d48e6289741eaddf839669452
Victor Vasiliev <vasilvv@mit.edu>
2013-08-25 06:52:35 -0400

    Release python-discuss 1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='discuss',
-      version='1.0',
+      version='1.1',
       description='Python client for Project Athena forum system',
       author='Victor Vasiliev',
       packages=['discuss']
